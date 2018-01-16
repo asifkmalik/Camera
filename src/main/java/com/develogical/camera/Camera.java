928bcb8b049b5b778f9be9dd352ab10061aaa684
Asif Malik <asif.malik@gmail.com>
2018-01-16 16:13:44 +0000

    Camera off powers down the sensor

--- src/main/java/com/develogical/camera/Camera.java
+++ src/main/java/com/develogical/camera/Camera.java
@@ -2,16 +2,22 @@ package com.develogical.camera;
 
 public class Camera {
 
+    private Sensor sensor;
+
+    public Camera(Sensor sensor) {
+        this.sensor = sensor;
+    }
+
     public void pressShutter() {
         // not implemented
     }
 
     public void powerOn() {
-        // not implemented
+        this.sensor.powerUp();
     }
 
     public void powerOff() {
-       // not implemented
+       this.sensor.powerDown();
     }
 }
 
--- src/test/java/com/develogical/camera/CameraTest.java
+++ src/test/java/com/develogical/camera/CameraTest.java
@@ -6,8 +6,19 @@ import org.mockito.ArgumentCaptor;
 import static org.mockito.Mockito.*;
 
 public class CameraTest {
+    Sensor mockSensor = mock(Sensor.class);
+    Camera camera = new Camera(mockSensor);
+
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-        // write your test here
+        camera.powerOn();
+        verify(mockSensor).powerUp();
     }
+
+    @Test
+    public void switchingTheCameraOffPowersDownTheSensor() {
+        camera.powerOff();
+        verify(mockSensor).powerDown();
+    }
+
 }
